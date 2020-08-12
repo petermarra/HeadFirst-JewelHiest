f607ecf2f70bbd91394375a4e12f865e0aad8d7d
Peter Marra <neverquit@roadrunner.com>
2020-08-12 19:38:10 -0400

    initial load

---
+++ JewelHiest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JewelHiest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
