dfc7d9886d023a3c67afc093a36d1d27c19253c1
Peter Marra <neverquit@roadrunner.com>
2020-08-14 20:41:10 -0400

    add classes add override emthods

---
+++ JewelHiest/Jewels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace JewelHiest
+{
+    class Jewels
+    {
+        public string Sparkle()
+        {
+            return "Sparkle, sparkle";
+        }
+    }
+}
---
+++ JewelHiest/Safe.cs
@@ -0,0 +1,23 @@
+﻿namespace JewelHiest
+{
+    class Safe
+    {
+        private Jewels contents = new Jewels();
+        private string safeCombination = "1234";
+        public Jewels Open(string combination)
+        {
+            if (combination == safeCombination)
+            {
+                return contents;
+            }
+            else
+                return null;
+        }
+
+        public void PickLock(LockSmith lockpicker)
+        {
+            lockpicker.WriteDownCombination(safeCombination);
+        }
+
+    }
+}
